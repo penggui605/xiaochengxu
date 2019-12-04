0ff04483d0afe4007dacb73c67ffdc50940e1048
penggui605 <744202754@qq.com>
2019-12-04 14:15:22 +0800

    1

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -5,10 +5,20 @@
         <i-grid-icon>
             <image src="/static/images/user.png" />
         </i-grid-icon>
-        <i-grid-label>Grid</i-grid-label>
+        <i-grid-label>免费</i-grid-label>
     </i-grid-item>
-    <i-grid-item>Grid</i-grid-item>
-    <i-grid-item>Grid</i-grid-item>
+    <i-grid-item>
+        <i-grid-icon>
+            <image src="/static/images/user.png" />
+        </i-grid-icon>
+        <i-grid-label>男频</i-grid-label>
+    </i-grid-item>
+    <i-grid-item>
+        <i-grid-icon>
+            <image src="/static/images/user.png" />
+        </i-grid-icon>
+        <i-grid-label>女频</i-grid-label>
+    </i-grid-item>
   </i-grid>
   </div>
 </template>
